68dbd29703f9f9f4100e580c936a5e1132062473
TiNredmc <thipok17@gmail.com>
2021-03-28 09:34:25 +0700

    Header file for Sharp MEM driver
    Signed-off-by: TiNredmc <thipok17@gmail.com>

---
+++ include/sharp_memlcd.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Sharp Memory Display Driver.
+ *
+ * Copyright (c) 2020 Thipok Jiamjarapan
+ * <thipok17@gmail.com> 
+ *
+ */
+#ifndef SHARP_MEMLCD_H_
+#define SHARP_MEMLCD_H_
+
+
+
+#endif
